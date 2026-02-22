3b8eef33bae41dae3f2ae6b1ff534961490ee309
MacBook Pro <macbookpro@MacBookPro.lan>
2026-02-22 14:15:56 +0500

    new section: added about section

---
+++ src/components/section/About/About.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <section class="about">
+    <div class="about__content">
+      <h1 class="main__title">Biz haqimizda</h1>
+      <div class="about__content-box">
+        <div class="about__content-l">
+          <img src="@/assets/images/nophoto.jpg" width="100%" alt="" />
+        </div>
+        <div class="about__content-1 about__content-grid">
+          <p>Faoliyat davri</p>
+          <h2>4 yil</h2>
+          <span>since 2024</span>
+        </div>
+        <div class="about__content-2 about__content-grid">
+          <p>Xodimlar</p>
+          <h2>20+</h2>
+        </div>
+        <div class="about__content-3 about__content-grid">
+          <p>Kurslar</p>
+          <h2>10+</h2>
+        </div>
+        <div class="about__content-4 about__content-grid">
+          <p>O'quvchilar</p>
+          <h2>500+</h2>
+        </div>
+        <div class="about__content-5 about__content-grid">
+          <p>Bitiruvchilar</p>
+          <h2>1000+</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from "@/components/section/Header/Header.vue"
+import About from "@/components/section/About/About.vue"
 </script>
 
 <template>
      <Header />
+     <About />
 </template>
